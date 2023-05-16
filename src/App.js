9da719182662d040b51b4f64a8925e1b39574edc
Carmen <liz.ocotan.clno@gmail.com>
2023-05-16 13:41:48 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ExportApi from "./componentes/ExportApi";
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <h1 className="title">RoboFriends</h1>
+      <ExportApi/>
+    </div>
+  );
+}
+
+export default App;
+
